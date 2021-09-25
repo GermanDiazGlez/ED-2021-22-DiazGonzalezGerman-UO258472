35e288b90d885fe7fa06ff0347dfaaffee0ce8bf
UO258472 <uo258472@uniovi.es>
2021-09-25 20:11:15 +0200

    Algoritmos acabado

--- src/p1Algoritmia/Algorithms.java
+++ src/p1Algoritmia/Algorithms.java
@@ -14,8 +14,10 @@ public class Algorithms {
 	}
 
 	
-	/*
+	/**
 	 * Calcula el factorial de n>=0 de forma recursiva
+	 * @param n
+	 * @return valor factorial(n)
 	 */
 	public long factorial (int n) { 
 		doNothing();
@@ -29,8 +31,10 @@ public class Algorithms {
 		return n * factorial(n-1);
 		} 
 	
-	/*
+	/**
 	 * Calcula el termino n-esimo teniendo en cuenta que el primer término se considera fib(0)=0; y fib(1)=1 
+	 * @param n
+	 * @return valor fibonacci(n)
 	 */
 	public int fibonacci (int n) {
 		doNothing();
@@ -49,8 +53,10 @@ public class Algorithms {
 		return fibonacci(n-1) + fibonacci(n-2);
 		} 
 	
-	/*
+	/**
 	 * Calcula 2 elevado a n de forma recursiva 2^n = 2*2^(n-1) 
+	 * @param n
+	 * @return valor 2^n
 	 */
 	public long pow2Rec1(int n) {
 		doNothing();
@@ -65,8 +71,10 @@ public class Algorithms {
 		return 2 * pow2Rec1(n-1);
 	} 
 	
-	/*
+	/**
 	 * Calcula 2 elevado a n de forma recursiva 2^n = 2^(n-1) + 2^(n-1) 
+	 * @param n
+	 * @return valor 2^n
 	 */
 	public long pow2Rec2(int n) {
 		doNothing();
@@ -81,8 +89,10 @@ public class Algorithms {
 		return pow2Rec2(n-1) + pow2Rec2(n-1);
 	}
 	
-	/*
-	 * Calcula 2 elevado a n de forma recursiva 2^n = 2^(n/2) * 2^(n/2) 
+	/**
+	 * Calcula 2 elevado a n de forma recursiva 2^n = 2^(n/2) * 2^(n/2)
+	 * @param n
+	 * @return valor 2^n
 	 */
 	public long pow2Rec3(int n) {
 		doNothing();
@@ -101,8 +111,10 @@ public class Algorithms {
 		}
 	}
 	
-	/*
+	/**
 	 * Calcula 2 elevado a n de forma recursiva 2^n = como pow2Rec3 pero sin repetir llamada
+	 * @param n
+	 * @return valor 2^n
 	 */
 	public long pow2Rec4(int n) {
 		doNothing();
@@ -123,8 +135,10 @@ public class Algorithms {
 		}
 	}
 	
-	/*
+	/**
 	 * Calcula 2 elevado a n de forma iterativa 
+	 * @param n
+	 * @return valor 2^n
 	 */
 	public long pow2Iter (int n) {
 	    if (n<0){
@@ -148,10 +162,14 @@ public class Algorithms {
 	    return res;
 	}
 	
-	/*
+	/**
 	 * Calcula a elevado a b de forma recursiva por potencias sucesivas
+	 * @param a
+	 * @param b
+	 * @return valor a^b
 	 */
 	public int potenciaRec (int a, int b){
+		doNothing();
 		if (b<0 || a<0){
 			throw new InvalidParameterException("No permitido alguno de los parámetros introducidos");
 		}
@@ -167,10 +185,14 @@ public class Algorithms {
 		return a * potenciaRec(a, b-1);
 	}
 	
-	/*
+	/**
 	 * Calcula el resto de la división a/b de forma recursiva
+	 * @param a
+	 * @param b
+	 * @return resto a/b
 	 */
 	public int restoDivRec (int a, int b){
+		doNothing();
 		if (b<0 || a<0){
 			throw new InvalidParameterException("No permitido alguno de los parámetros introducidos");
 		}
@@ -186,10 +208,14 @@ public class Algorithms {
 		return restoDivRec(a-b, b);
 	}
 	
-	/*
+	/**
 	 * Calcula la división entera a/b de forma recursiva
+	 * @param a
+	 * @param b
+	 * @return cociente a/b
 	 */
 	public int divEntRec (int a, int b){
+		doNothing();
 		if (b<0 || a<0){
 			throw new InvalidParameterException("No permitido alguno de los parámetros introducidos");
 		}
@@ -205,19 +231,9 @@ public class Algorithms {
 		return 1 + divEntRec(a-b, b);
 	}
 	
-	/*
-	 * Invierte un entero de forma recursiva
-	 */
-	public String invertirEnteroRec(int num) {
-        if(num<10){
-            return String.valueOf(num);
-        } else {
-            return String.valueOf(num%10) + invertirEnteroRec(num/10);
-        }
-    } 
-	
-	/*
+	/**
 	 * Algoritmo con una complejidad temporal lineal
+	 * @param n
 	 */
 	public void linear (int n) {
 		if (n<0){
@@ -229,8 +245,9 @@ public class Algorithms {
 		}
 	}
 	
-	/*
+	/**
 	 * Algoritmo con una complejidad temporal cuadrática 
+	 * @param n
 	 */
 	public void quadratic(int n) {
 		if (n<0){
@@ -244,8 +261,9 @@ public class Algorithms {
 		}
 	} 
 	
-	/*
+	/**
 	 * Algoritmo con una complejidad temporal cúbica
+	 * @param n
 	 */
 	public void cubic(int n) { 
 		if (n<0){
@@ -274,5 +292,18 @@ public class Algorithms {
 		    doNothing();
 		}
 	} 
+	
+	/**
+	 * Invierte un entero de forma recursiva
+	 * @param num
+	 * @return String del numero invertido
+	 */
+	public String invertirEnteroRec(int num) {
+        if(num<10){
+            return String.valueOf(num);
+        } else {
+            return String.valueOf(num%10) + invertirEnteroRec(num/10);
+        }
+    } 
 	
 }
--- src/p1Algoritmia/AlgorithmsTest.java
+++ src/p1Algoritmia/AlgorithmsTest.java
@@ -13,6 +13,9 @@ public class AlgorithmsTest {
 	public void testFact() {
 		assertEquals(6, alg.factorial(3));
 		assertEquals(24, alg.factorial(4));
+		assertEquals(40320, alg.factorial(8));
+		assertEquals(5040, alg.factorial(7));
+		assertEquals(120, alg.factorial(5));
 	}
 	
 	@Test
@@ -27,7 +30,7 @@ public class AlgorithmsTest {
 	}
 	
 	@Test
-	public void testPow2() {
+	public void testPow1() {
 		assertEquals(1, alg.pow2Rec1(0));
 		assertEquals(2, alg.pow2Rec1(1));
 		assertEquals(4, alg.pow2Rec1(2));
@@ -37,6 +40,43 @@ public class AlgorithmsTest {
 		assertEquals(64, alg.pow2Rec1(6));
 		assertNotEquals(0, alg.pow2Rec1(60));
 		assertEquals(0, alg.pow2Rec1(100));
+	}
+	
+	@Test
+	public void testPow2() {
+		assertEquals(1, alg.pow2Rec2(0));
+		assertEquals(2, alg.pow2Rec2(1));
+		assertEquals(4, alg.pow2Rec2(2));
+		assertEquals(8, alg.pow2Rec2(3));
+		assertEquals(16, alg.pow2Rec2(4));
+		assertEquals(32, alg.pow2Rec2(5));
+		assertEquals(64, alg.pow2Rec2(6));
+	}
+	
+	@Test
+	public void testPow3() {
+		assertEquals(1, alg.pow2Rec3(0));
+		assertEquals(2, alg.pow2Rec3(1));
+		assertEquals(4, alg.pow2Rec3(2));
+		assertEquals(8, alg.pow2Rec3(3));
+		assertEquals(16, alg.pow2Rec3(4));
+		assertEquals(32, alg.pow2Rec3(5));
+		assertEquals(64, alg.pow2Rec3(6));
+		assertNotEquals(0, alg.pow2Rec3(60));
+		assertEquals(0, alg.pow2Rec3(100));
+	}
+	
+	@Test
+	public void testPow4() {
+		assertEquals(1, alg.pow2Rec4(0));
+		assertEquals(2, alg.pow2Rec4(1));
+		assertEquals(4, alg.pow2Rec4(2));
+		assertEquals(8, alg.pow2Rec4(3));
+		assertEquals(16, alg.pow2Rec4(4));
+		assertEquals(32, alg.pow2Rec4(5));
+		assertEquals(64, alg.pow2Rec4(6));
+		assertNotEquals(0, alg.pow2Rec4(60));
+		assertEquals(0, alg.pow2Rec4(100));
 	}
 	
 	@Test
